096203a18a0f8b02e85545ec5b92923d61e260b4
Hanseok <hasuikhs@gmail.com>
2020-08-09 21:37:31 +0900

    200809|Update python list comprehension

--- Algorithm/Problem/05_3과5배수.py
+++ Algorithm/Problem/05_3과5배수.py
@@ -1,10 +1,3 @@
-def function(n):
-    i = 1
-    while i <= n:
-        if (i % 3 == 0) & (i % 5 == 0):
-            print(f'{i}는 3과 5의 배수')
-        i += 1
+[n * 2 for n in range(1, 10 + 1) if n % 2 == 1]
 
-if __name__ == '__main__':
-    function(100)
-        
+print('asdf')
