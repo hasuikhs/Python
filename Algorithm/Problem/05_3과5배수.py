e35b1302bb1b53e03a0e8f3c43361f5a336ddfc2
Hanseok <hasuikhs@gmail.com>
2020-08-30 22:32:46 +0900

    200830|Add python exception

--- Algorithm/Problem/05_3과5배수.py
+++ Algorithm/Problem/05_3과5배수.py
@@ -1,7 +1,17 @@
-a = []
 
-a[] = 1
 
-a[] = 2
+def reorderLogFiles(self, logs: List[str]) -> List[str]:
+    letters, digits = [], []
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+        
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    
+    return letters + digits
 
-print(a)
+logs = ["dig1 8 1 5 1", "let1 art can", "dig2 3 6", "let2 own kit dig", "let3 art zero"]
+
+reorderLogFiles(self, logs)
